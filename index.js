97c26417b4d18ec21c1d81e76408a366e5ddd2e1
Jacky Lin <jacky.jia.lin@gmail.com>
2020-06-23 09:14:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return `Happy holidarys, ${name}!`
+  return `Happy holidays, ${name}!`
 }
 
 function happyCustomHolidayTo(holiday, name) {
